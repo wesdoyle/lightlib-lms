d00d90bdd54c22572cc102538921348574e78fa8
Wes D <wesdoyle08@gmail.com>
2018-02-28 20:41:14 -0600

    Complete migration to dotnet 2.0

--- Library/Startup.cs
+++ Library/Startup.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using LibraryData;
 using LibraryService;
 
@@ -11,58 +11,36 @@ namespace Library
 {
     public class Startup
     {
-        // add a constructor, and 
-        // create an instance of a 
-        // configuration builder.
-        // use it to configure the various 
-        // configuration sources for the application.
-        public Startup(IHostingEnvironment env)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables(); // could add connection strings here.
-            // here it is.q
-            Configuration = builder.Build();
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(env.ContentRootPath)
+            //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            //    .AddEnvironmentVariables();
+            //Configuration = builder.Build();
+            Configuration = config;
         }
 
-        // access the configuration in a property.
-        public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
             services.AddSingleton(Configuration);
-            services.AddScoped<ILibraryCard, LibraryCardService>(); // so that Bookservice is injected into controllers and other components that request IBook
-            services.AddScoped<ILibraryBranch, LibraryBranchService>(); // so that Bookservice is injected into controllers and other components that request IBook
-            services.AddScoped<IPatron, PatronService>(); // so that Bookservice is injected into controllers and other components that request IBook
-            services.AddScoped<ICheckout, CheckoutService>(); // so that Bookservice is injected into controllers and other components that request IBook
-            services.AddScoped<ILibraryAsset, LibraryAssetService>(); // so that Bookservice is injected into controllers and other components that request IBook
-            services.AddScoped<IBook, BookService>(); // so that Bookservice is injected into controllers and other components that request IBook
-            services.AddScoped<IVideo, VideoService>(); // so that Bookservice is injected into controllers and other components that request IBook
-            services.AddScoped<IStatus, StatusService>(); // so that Bookservice is injected into controllers and other components that request IBook
+            services.AddScoped<ILibraryCard, LibraryCardService>();
+            services.AddScoped<ILibraryBranch, LibraryBranchService>();
+            services.AddScoped<IPatron, PatronService>(); 
+            services.AddScoped<ICheckout, CheckoutService>(); 
+            services.AddScoped<ILibraryAsset, LibraryAssetService>(); 
+            services.AddScoped<IBook, BookService>(); 
+            services.AddScoped<IVideo, VideoService>(); 
+            services.AddScoped<IStatus, StatusService>(); 
 
-            // configure ef and dbcontext.
-            // ef can now work with other databases, including non-relational
             services.AddDbContext<LibraryDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
-
-            // Now we can use EF to generate our database in a two-step process.
-            // Write migration code we can execute to create a database and a schema.
-            // ef can generate migration code by looking at a database and seeing its
-            // current state.
-
-            // Install NuGet package EntityFrameworkCore.Tools
-            // in the package manager console.
-            // console command - add-migration
-            // console command - update-databse
-
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
