7305b20180ae612a83e7fa94e01c7215789d8b12
Wes D <wesdoyle08@gmail.com>
2018-03-01 20:21:15 -0600

    Update tests

--- Library.Tests/BookServiceShould.cs
+++ Library.Tests/BookServiceShould.cs
@@ -114,5 +114,82 @@ namespace Library.Tests
             queryResult.Should().HaveCount(2);
             queryResult.Should().Contain(a => a.DeweyIndex == "SNO");
         }
+
+        [Test]
+        public void Return_Book_By_Author()
+        {
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Author = "Herman Hesse",
+                    Title = "Siddhartha"
+                },
+
+                new Book
+                {
+                    Author = "Herman Hesse",
+                    Title = "Knulp"
+                },
+
+                new Book
+                {
+                    Author = "Thomas Mann",
+                    Title = "The Magic Mountain"
+                }
+
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Book>>();
+            mockSet.As<IQueryable<Book>>().Setup(b => b.Provider).Returns(books.Provider);
+            mockSet.As<IQueryable<Book>>().Setup(b => b.Expression).Returns(books.Expression);
+            mockSet.As<IQueryable<Book>>().Setup(b => b.ElementType).Returns(books.ElementType);
+            mockSet.As<IQueryable<Book>>().Setup(b => b.GetEnumerator()).Returns(books.GetEnumerator);
+
+            var mockCtx = new Mock<LibraryDbContext>();
+            mockCtx.Setup(c => c.Books).Returns(mockSet.Object);
+
+            var sut = new BookService(mockCtx.Object);
+            var queryResult = sut.GetByAuthor("Herman Hesse").ToList();
+
+            queryResult.Should().AllBeOfType(typeof(Book));
+            queryResult.Should().HaveCount(2);
+            queryResult.Should().Contain(b => b.Title == "Siddhartha");
+            queryResult.Should().Contain(b => b.Title == "Knulp");
+        }
+
+        [Test]
+        public void Return_Book_By_ISBN()
+        {
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    ISBN = "123a",
+                    Title = "Pale Fire"
+                },
+                new Book
+                {
+                    ISBN = "zz09",
+                    Title = "War and Peace"
+                }
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Book>>();
+            mockSet.As<IQueryable<Book>>().Setup(b => b.Provider).Returns(books.Provider);
+            mockSet.As<IQueryable<Book>>().Setup(b => b.Expression).Returns(books.Expression);
+            mockSet.As<IQueryable<Book>>().Setup(b => b.ElementType).Returns(books.ElementType);
+            mockSet.As<IQueryable<Book>>().Setup(b => b.GetEnumerator()).Returns(books.GetEnumerator);
+
+            var mockCtx = new Mock<LibraryDbContext>();
+            mockCtx.Setup(c => c.Books).Returns(mockSet.Object);
+
+            var sut = new BookService(mockCtx.Object);
+            var queryResult = sut.GetByIsbn("123a").ToList();
+
+            queryResult.Should().AllBeOfType(typeof(Book));
+            queryResult.Should().HaveCount(1);
+            queryResult.Should().Contain(b => b.Title == "Pale Fire");
+        }
     }
 }
