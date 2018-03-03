cb7a796ac77d03a8858b4b75737e5bff5f563400
Wes D <wesdoyle08@gmail.com>
2018-03-02 22:50:16 -0600

    Start CheckoutService tests

--- Library.Tests/CheckoutServiceShould.cs
+++ Library.Tests/CheckoutServiceShould.cs
@@ -1,12 +1,28 @@
-﻿using NUnit.Framework;
+﻿using LibraryData;
+using LibraryData.Models;
+using LibraryService;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
 
 namespace Library.Tests
 {
+    [TestFixture]
     public class CheckoutServiceShould
     {
-        public void UpdateItemStatusToCheckedOut_When_ItemCheckedOut()
+        [Test]
+        public void Add_Checkout()
         {
-            Assert.Fail("Finish test");
+            var mockSet = new Mock<DbSet<Book>>();
+            var mockCtx = new Mock<LibraryDbContext>();
+
+            mockCtx.Setup(c => c.Books).Returns(mockSet.Object);
+            var sut = new CheckoutService(mockCtx.Object);
+
+            sut.Add(new Checkout());
+
+            mockCtx.Verify(s => s.Add(It.IsAny<Checkout>()), Times.Once());
+            mockCtx.Verify(c => c.SaveChanges(), Times.Once());
         }
     }
 }
