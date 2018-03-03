de3695b0b7a1b521b05fe411805e2c5b6e8991f8
Wes D <wesdoyle08@gmail.com>
2018-03-03 11:54:28 -0600

    Create code coverage batch file, update packages, gitignore

---
+++ _coverage.bat
@@ -0,0 +1,7 @@
+OpenCover.Console.exe  -target:"c:\program files\dotnet\dotnet.exe" ^
+-targetargs:"test \"Library.Tests\Library.Tests.csproj\" -c Debug --no-build" ^
+-mergeoutput -hideskipped:File ^
+-output:coverage\coverage.xml ^
+-oldStyle ^
+-filter:"+[Lib*]* -[Library.Tests*]* -[LibraryData*]*" ^
+-register:user
