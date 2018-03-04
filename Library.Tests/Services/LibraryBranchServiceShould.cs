5a4b7059db9077168845ecdfb54275a5f7af627c
Wes D <wesdoyle08@gmail.com>
2018-03-04 10:32:07 -0600

    Write Library Branch tests

--- Library.Tests/Services/LibraryBranchServiceShould.cs
+++ Library.Tests/Services/LibraryBranchServiceShould.cs
@@ -98,7 +98,7 @@ namespace Library.Tests.Services
         }
 
         [Test]
-        public void Get_Asset_Count()
+        public void Get_Branch_Asset_Count()
         {
             var branches = new List<LibraryBranch>
             {
@@ -141,19 +141,35 @@ namespace Library.Tests.Services
         }
 
         [Test]
-        public void Get_LibraryBranch_By_Isbn()
+        public void Get_Branch_Assets()
         {
             var branches = new List<LibraryBranch>
             {
                 new LibraryBranch
                 {
+                    Id = -86,
+                    LibraryAssets = new List<LibraryAsset>
+                    {
+                        new Book
+                        {
+                            Id = 11
+                        },
+
+                        new Video
+                        {
+                            Id = 234
+                        },
+
+                        new Book
+                        {
+                            Id = -145
+                        }
+                    }
                 },
-                new LibraryBranch
-                {
-                }
             }.AsQueryable();
 
             var mockSet = new Mock<DbSet<LibraryBranch>>();
+
             mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.Provider).Returns(branches.Provider);
             mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.Expression).Returns(branches.Expression);
             mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.ElementType).Returns(branches.ElementType);
@@ -163,10 +179,137 @@ namespace Library.Tests.Services
             mockCtx.Setup(c => c.LibraryBranches).Returns(mockSet.Object);
 
             var sut = new LibraryBranchService(mockCtx.Object);
-            //var queryResult = sut.GetByIsbn("123a").ToList();
-            //queryResult.Should().AllBeOfType(typeof(LibraryBranch));
-            //queryResult.Should().HaveCount(1);
-            //queryResult.Should().Contain(b => b.Title == "Pale Fire");
+            var queryResult = sut.GetAssets(branches.First().Id).ToList();
+            queryResult.Count.Should().Be(3);
+            queryResult.Should().Contain(q => q.Id == -145);
+        }
+
+        [Test]
+        public void Get_Branch_Assets_Value()
+        {
+            var branches = new List<LibraryBranch>
+            {
+                new LibraryBranch
+                {
+                    Id = -86,
+                    LibraryAssets = new List<LibraryAsset>
+                    {
+                        new Book
+                        {
+                            Id = 11,
+                            Cost = 12.50M
+                        },
+
+                        new Video
+                        {
+                            Id = 234,
+                            Cost = 100M
+                        },
+
+                        new Book
+                        {
+                            Id = -145,
+                            Cost = 10M
+                        }
+                    }
+                },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<LibraryBranch>>();
+
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.Provider).Returns(branches.Provider);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.Expression).Returns(branches.Expression);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.ElementType).Returns(branches.ElementType);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.GetEnumerator()).Returns(branches.GetEnumerator);
+
+            var mockCtx = new Mock<LibraryDbContext>();
+            mockCtx.Setup(c => c.LibraryBranches).Returns(mockSet.Object);
+
+            var sut = new LibraryBranchService(mockCtx.Object);
+            var queryResult = sut.GetAssetsValue(branches.First().Id);
+            queryResult.Should().Be(122.50M);
+        }
+
+        [Test]
+        public void Get_Patron_Count()
+        {
+            var branches = new List<LibraryBranch>
+            {
+                new LibraryBranch
+                {
+                    Id = 1,
+                    Patrons = new List<Patron>
+                    {
+                        new Patron 
+                        {
+                            Id = 11,
+                            FirstName = "Jin"
+                        },
+
+                        new Patron
+                        {
+                            Id = 234,
+                            FirstName = "Helen" 
+                        }
+                    }
+                },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<LibraryBranch>>();
+
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.Provider).Returns(branches.Provider);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.Expression).Returns(branches.Expression);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.ElementType).Returns(branches.ElementType);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.GetEnumerator()).Returns(branches.GetEnumerator);
+
+            var mockCtx = new Mock<LibraryDbContext>();
+            mockCtx.Setup(c => c.LibraryBranches).Returns(mockSet.Object);
+
+            var sut = new LibraryBranchService(mockCtx.Object);
+            var queryResult = sut.GetPatronCount(branches.First().Id);
+            queryResult.Should().Be(2);
+        }
+
+        [Test]
+        public void Get_Patrons_Associated_With_Branch()
+        {
+            var branches = new List<LibraryBranch>
+            {
+                new LibraryBranch
+                {
+                    Id = 1,
+                    Patrons = new List<Patron>
+                    {
+                        new Patron 
+                        {
+                            Id = 11,
+                            FirstName = "Jin"
+                        },
+
+                        new Patron
+                        {
+                            Id = 234,
+                            FirstName = "Helen" 
+                        }
+                    }
+                },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<LibraryBranch>>();
+
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.Provider).Returns(branches.Provider);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.Expression).Returns(branches.Expression);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.ElementType).Returns(branches.ElementType);
+            mockSet.As<IQueryable<LibraryBranch>>().Setup(b => b.GetEnumerator()).Returns(branches.GetEnumerator);
+
+            var mockCtx = new Mock<LibraryDbContext>();
+            mockCtx.Setup(c => c.LibraryBranches).Returns(mockSet.Object);
+
+            var sut = new LibraryBranchService(mockCtx.Object);
+            var queryResult = sut.GetPatrons(branches.First().Id).ToList();
+            queryResult.Count.Should().Be(2);
+            queryResult.Should().Contain(c => c.FirstName == "Helen");
+            queryResult.Should().Contain(c => c.FirstName == "Jin");
         }
     }
 }
