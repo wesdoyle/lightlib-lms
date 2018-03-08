74933370084ef514f3568871222aad4106b87b0c
Wes D <wesdoyle08@gmail.com>
2018-03-07 22:47:44 -0600

    Remove currently unused model, start CatalogController tests

---
+++ Library.Tests/Controllers/CatalogControllerShould.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Library.Data;
+using Library.Data.Models;
+using Library.Web.Controllers;
+using Library.Web.Models.Catalog;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace Library.Tests.Controllers
+{
+    [TestFixture]
+    public class CatalogControllerShould
+    {
+        [Test]
+        public void Return_Catalog_Index_View()
+        {
+            var mockLibraryAssetService = new Mock<ILibraryAssetService>();
+            var mockCheckoutService = new Mock<ICheckoutService>();
+
+            mockLibraryAssetService.Setup(r => r.GetAll()).Returns(GetAllAssets());
+
+            var controller = new CatalogController(mockLibraryAssetService.Object, mockCheckoutService.Object);
+
+            var result = controller.Index();
+            var viewResult = result.Should().BeOfType<ViewResult>();
+            var viewModel = viewResult.Subject.ViewData.Model.Should().BeAssignableTo<AssetIndexModel>();
+            viewModel.Subject.Assets.Count().Should().Be(2);
+        }
+
+        [Test]
+        public void Return_AssetIndexListingModel()
+        {
+            var mockLibraryAssetService = new Mock<ILibraryAssetService>();
+            var mockCheckoutService = new Mock<ICheckoutService>();
+
+            mockLibraryAssetService.Setup(r => r.GetAll()).Returns(GetAllAssets());
+
+            var controller = new CatalogController(mockLibraryAssetService.Object, mockCheckoutService.Object);
+
+            var result = controller.Index();
+            var viewResult = result.Should().BeOfType<ViewResult>();
+            viewResult.Subject.Model.Should().BeOfType<AssetIndexModel>();
+        }
+
+        [Test]
+        public void Return_Branch_Detail_View()
+        {
+            var mockLibraryAssetService = new Mock<ILibraryAssetService>();
+            var mockCheckoutService = new Mock<ICheckoutService>();
+
+            mockLibraryAssetService.Setup(r => r.Get(24)).Returns(GetAsset());
+            mockLibraryAssetService.Setup(r => r.GetCurrentLocation(24)).Returns(new LibraryBranch
+            {
+                Name = "Main St. Library"
+            });
+
+            var controller = new CatalogController(mockLibraryAssetService.Object, mockCheckoutService.Object);
+
+            var result = controller.Detail(24);
+
+            var viewResult = result.Should().BeOfType<ViewResult>();
+            var viewModel = viewResult.Subject.ViewData.Model.Should().BeAssignableTo<AssetDetailModel>();
+
+            viewModel.Subject.Title.Should().Be("Orlando");
+        }
+
+        [Test]
+        public void Return_BranchDetailModel()
+        {
+            var mockLibraryAssetService = new Mock<ILibraryAssetService>();
+            var mockCheckoutService = new Mock<ICheckoutService>();
+
+            mockLibraryAssetService.Setup(r => r.Get(24)).Returns(GetAsset());
+            mockLibraryAssetService.Setup(r => r.GetCurrentLocation(24)).Returns(new LibraryBranch
+            {
+                Name = "Main St. Library"
+            });
+            var controller = new CatalogController(mockLibraryAssetService.Object, mockCheckoutService.Object);
+            var result = controller.Detail(24);
+            var viewResult = result.Should().BeOfType<ViewResult>();
+
+            viewResult.Subject.Model.Should().BeOfType<AssetDetailModel>();
+        }
+
+        private static IEnumerable<LibraryAsset> GetAllAssets()
+        {
+            return new List<LibraryAsset>
+            {
+                new Book
+                {
+                    Title = "Orlando",
+                    Author = "Virginia Woolf",
+                    Status = new Status()
+                    {
+                        Name = "Checked In",
+                        Id = 1,
+                    },
+                },
+
+                new Video
+                {
+                    Title = "Happy People",
+                    Director = "Werner Herzog",
+                    ImageUrl = "images/sample.jpg",
+                    Status = new Status()
+                    {
+                        Name = "Lost",
+                        Id = 3,
+                    },
+                }
+            };
+        }
+
+        private static Book GetAsset()
+        {
+            return new Book
+            {
+                Title = "Orlando",
+                Author = "Virginia Woolf",
+                Status = new Status()
+                {
+                    Name = "Checked In",
+                    Id = 1,
+                }
+            };
+        }
+    }
+}
