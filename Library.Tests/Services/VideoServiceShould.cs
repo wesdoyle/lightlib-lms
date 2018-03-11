51cd005f747f68e6d9a0b9bd557565199f402c5c
Wes D <wesdoyle08@gmail.com>
2018-03-11 13:39:14 -0500

    Add GetAll VideoService test

--- Library.Tests/Services/VideoServiceShould.cs
+++ Library.Tests/Services/VideoServiceShould.cs
@@ -18,7 +18,35 @@ namespace Library.Tests.Services
         [Test]
         public void Get_All_Videos()
         {
+            var videos = new List<Video>
+            {
+                new Video 
+                {
+                    Id = 1234
+                },
 
+                new Video 
+                {
+                    Id = -6
+                }
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Video>>();
+            mockSet.As<IQueryable<Video>>().Setup(b => b.Provider).Returns(videos.Provider);
+            mockSet.As<IQueryable<Video>>().Setup(b => b.Expression).Returns(videos.Expression);
+            mockSet.As<IQueryable<Video>>().Setup(b => b.ElementType).Returns(videos.ElementType);
+            mockSet.As<IQueryable<Video>>().Setup(b => b.GetEnumerator()).Returns(videos.GetEnumerator);
+
+            var mockCtx = new Mock<LibraryDbContext>();
+            mockCtx.Setup(c => c.Videos).Returns(mockSet.Object);
+
+            var sut = new VideoService(mockCtx.Object);
+            var queryResult = sut.GetAll().ToList();
+
+            queryResult.Should().AllBeOfType(typeof(Video));
+            queryResult.Should().HaveCount(2);
+            queryResult.Should().Contain(a => a.Id == -6);
+            queryResult.Should().Contain(a => a.Id == 1234);
         }
 
         [Test]
