37a9b1d6d3d8cdbe999b78690c64e83e33ebaf21
wesdoyle <wesdoyle08@gmail.com>
2020-08-03 20:19:55 -0500

    Update PatronService

--- Library.Service/PatronService.cs
+++ Library.Service/PatronService.cs
@@ -1,78 +1,249 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
 using Library.Data;
 using Library.Data.Models;
+using Library.Models;
+using Library.Models.DTOs;
 using Library.Service.Interfaces;
+using Library.Service.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Library.Service
-{
-    public class PatronService : IPatronService
-    {
+namespace Library.Service {
+    public class PatronService : IPatronService {
+        
         private readonly LibraryDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IPaginator<Patron> _patronPaginator;
+        private readonly IPaginator<CheckoutHistory> _historyPaginator;
+        private readonly IPaginator<Checkout> _checkoutPaginator;
+        private readonly IPaginator<Hold> _holdPaginator;
 
-        public PatronService(LibraryDbContext context)
-        {
+        /// <summary>
+        /// Handles Library Patron business logic
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="mapper"></param>
+        /// <param name="patronPaginator"></param>
+        /// <param name="historyPaginator"></param>
+        /// <param name="holdPaginator"></param>
+        public PatronService(
+            LibraryDbContext context,
+            IMapper mapper,
+            IPaginator<Patron> patronPaginator,
+            IPaginator<CheckoutHistory> historyPaginator ,
+            IPaginator<Checkout> checkoutPaginator ,
+            IPaginator<Hold> holdPaginator
+            ) {
             _context = context;
+            _mapper = mapper;
+            _patronPaginator = patronPaginator;
+            _historyPaginator = historyPaginator;
+            _holdPaginator = holdPaginator;
+            _checkoutPaginator = checkoutPaginator;
         }
 
-        public void Add(Patron newPatron)
-        {
-            _context.Add(newPatron);
-            _context.SaveChanges();
-        }
-
-        public Patron Get(int id)
-        {
-            return _context.Patrons
+        /// <summary>
+        /// Gets a Patron by ID
+        /// </summary>
+        /// <param name="patronId"></param>
+        /// <returns></returns>
+        public async Task<ServiceResult<PatronDto>> Get(int patronId) {
+            var patron = await _context.Patrons
                 .Include(a => a.LibraryCard)
                 .Include(a => a.HomeLibraryBranch)
-                .FirstOrDefault(p => p.Id == id);
+                .FirstAsync(p => p.Id == patronId);
+
+            var patronDto = _mapper.Map<PatronDto>(patron);
+            
+            return new ServiceResult<PatronDto> {
+                Data = patronDto,
+                Error = null
+            };
         }
 
-        public IEnumerable<Patron> GetAll()
-        {
-            return _context.Patrons
-                .Include(a => a.LibraryCard)
-                .Include(a => a.HomeLibraryBranch);
-            // Eager load this data.
+        /// <summary>
+        /// Creates a new Patron
+        /// </summary>
+        /// <param name="newPatron"></param>
+        /// <returns></returns>
+        public async Task<ServiceResult<int>> Add(Patron newPatron) {
+            await _context.AddAsync(newPatron);
+            var patronId = await _context.SaveChangesAsync();
+            return new ServiceResult<int> {
+                Data = patronId,
+                Error = null
+            };
         }
 
-        public IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId)
-        {
-            var cardId = _context.Patrons
-                .Include(a => a.LibraryCard)
-                .FirstOrDefault(a => a.Id == patronId)?
-                .LibraryCard.Id;
+        /// <summary>
+        /// Gets a paginated collection of Patrons
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="perPage"></param>
+        /// <returns></returns>
+        public async Task<PagedServiceResult<PatronDto>> GetAll(int page, int perPage) {
+            var patrons = _context.Patrons;
 
-            return _context.CheckoutHistories
+            var pageOfPatrons = await _patronPaginator 
+                .BuildPageResult(patrons, page, perPage, b => b.Id)
+                .ToListAsync();
+            
+            var paginatedPatrons = _mapper.Map<List<PatronDto>>(pageOfPatrons);
+            
+            var paginationResult = new PaginationResult<PatronDto> {
+                Results = paginatedPatrons,
+                PerPage = perPage,
+                PageNumber = page
+            };
+            
+            return new PagedServiceResult<PatronDto> {
+                Data = paginationResult,
+                Error = null
+            };
+        }
+
+        /// <summary>
+        /// Gets the paginated checkout history of a Patron by ID
+        /// </summary>
+        /// <param name="patronId"></param>
+        /// <param name="page"></param>
+        /// <param name="perPage"></param>
+        /// <returns></returns>
+        public async Task<PagedServiceResult<CheckoutHistoryDto>> GetCheckoutHistory(int patronId, int page,
+            int perPage) {
+            var patron = await _context.Patrons
+                .Include(a => a.LibraryCard)
+                .FirstAsync(a => a.Id == patronId);
+
+            if (patron == null) {
+                // TODO
+                return new PagedServiceResult<CheckoutHistoryDto> {
+                    Data = null,
+                    Error =  new ServiceError()
+                };
+            }
+
+            var cardId = patron.LibraryCard.Id;
+
+            var histories = _context.CheckoutHistories
                 .Include(a => a.LibraryCard)
                 .Include(a => a.LibraryAsset)
                 .Where(a => a.LibraryCard.Id == cardId)
                 .OrderByDescending(a => a.CheckedOut);
+
+            var pageOfHistories = await _historyPaginator
+                .BuildPageResult(histories, page, perPage, b => b.CheckedOut)
+                .ToListAsync();
+
+            var paginatedHistory = _mapper.Map<List<CheckoutHistoryDto>>(pageOfHistories);
+
+            var paginationResult = new PaginationResult<CheckoutHistoryDto> {
+                Results = paginatedHistory,
+                PerPage = perPage,
+                PageNumber = page
+            };
+
+            return new PagedServiceResult<CheckoutHistoryDto> {
+                Data = paginationResult,
+                Error = null
+            };
         }
 
-        public IEnumerable<Checkout> GetCheckouts(int id)
-        {
-            var patronCardId = Get(id).LibraryCard.Id;
-            return _context.Checkouts
+        /// <summary>
+        /// Gets the Holds currently held by a Patron
+        /// </summary>
+        /// <param name="patronId"></param>
+        /// <param name="page"></param>
+        /// <param name="perPage"></param>
+        /// <returns></returns>
+        public async Task<PagedServiceResult<HoldDto>> GetHolds(int patronId, int page, int perPage) {
+            var patron = await _context.Patrons
+                .Include(a => a.LibraryCard)
+                .FirstAsync(a => a.Id == patronId);
+                
+            if (patron == null) {
+                // TODO
+                return new PagedServiceResult<HoldDto> {
+                    Data = null,
+                    Error =  new ServiceError()
+                };
+            }
+                
+            var libraryCardId = patron.LibraryCard.Id;
+
+            var holds = _context.Holds
                 .Include(a => a.LibraryCard)
                 .Include(a => a.LibraryAsset)
-                .Where(v => v.LibraryCard.Id == patronCardId);
-        }
-
-        public IEnumerable<Hold> GetHolds(int patronId)
-        {
-            var cardId = _context.Patrons
-                .Include(a => a.LibraryCard)
-                .FirstOrDefault(a => a.Id == patronId)?
-                .LibraryCard.Id;
-
-            return _context.Holds
-                .Include(a => a.LibraryCard)
-                .Include(a => a.LibraryAsset)
-                .Where(a => a.LibraryCard.Id == cardId)
+                .Where(a => a.LibraryCard.Id == libraryCardId)
                 .OrderByDescending(a => a.HoldPlaced);
+
+            var pageOfHolds = await _holdPaginator 
+                .BuildPageResult(holds, page, perPage, b => b.Id)
+                .ToListAsync();
+
+            var paginatedHolds = _mapper.Map<List<HoldDto>>(pageOfHolds);
+
+            var paginationResult = new PaginationResult<HoldDto> {
+                Results = paginatedHolds,
+                PerPage = perPage,
+                PageNumber = page
+            };
+
+            return new PagedServiceResult<HoldDto> {
+                Data = paginationResult,
+                Error = null
+            };
+        }
+
+        /// <summary>
+        /// Gets a paginated collection of the provided Patron's Checkouts 
+        /// </summary>
+        /// <param name="patronId"></param>
+        /// <param name="page"></param>
+        /// <param name="perPage"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<PagedServiceResult<CheckoutDto>> GetCheckouts(int patronId, int page, int perPage) {
+            
+            var patron = await _context.Patrons
+                .Include(a => a.LibraryCard)
+                .FirstAsync(a => a.Id == patronId);
+                
+            if (patron == null) {
+                // TODO
+                return new PagedServiceResult<CheckoutDto> {
+                    Data = null,
+                    Error =  new ServiceError()
+                };
+            }
+                
+            var libraryCardId = patron.LibraryCard.Id;
+
+            var checkouts = _context.Checkouts
+                .Include(a => a.LibraryCard)
+                .Include(a => a.LibraryAsset)
+                .Where(a => a.LibraryCard.Id == libraryCardId)
+                .OrderByDescending(a => a.Since);
+
+            var pageOfCheckouts = await _checkoutPaginator 
+                .BuildPageResult(checkouts, page, perPage, b => b.Id)
+                .ToListAsync();
+
+            var paginatedCheckouts = _mapper.Map<List<CheckoutDto>>(pageOfCheckouts);
+
+            var paginationResult = new PaginationResult<CheckoutDto> {
+                Results = paginatedCheckouts,
+                PerPage = perPage,
+                PageNumber = page
+            };
+
+            return new PagedServiceResult<CheckoutDto> {
+                Data = paginationResult,
+                Error = null
+            };
         }
     }
 }
